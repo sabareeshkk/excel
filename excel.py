812e3aa5bbb5985ba48c712dec79f969983cf333
sabz <sbareeshk1991@gmail.com>
2015-08-27 13:24:49 +0530

    updated view of dict

--- excel.py
+++ excel.py
@@ -6,10 +6,11 @@ wb = open_workbook('sample-data.xls')
 excel = {}
 
 for s in wb.sheets():
-    excel[str(wb.sheets().index(s))] = []
-    for row in xrange(s.nrows):
-        for col in xrange(s.ncols):
+    excel['sheet' + str(wb.sheets().index(s))] = {}
+    for col in xrange(s.ncols):
+        excel['sheet' + str(wb.sheets().index(s))]['col' + str(col)] = []
+        for row in xrange(s.nrows):
             value = (s.cell(row, col).value)
-            excel[str(wb.sheets().index(s))].append(value)
+            excel['sheet' + str(wb.sheets().index(s))]['col' + str(col)].append(value)
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(excel)
