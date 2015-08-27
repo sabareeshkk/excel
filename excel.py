80fdb3a8fbdbdb24f89716d203d390a1427b6f10
sabz <sbareeshk1991@gmail.com>
2015-08-27 14:14:14 +0530

    chages made

--- excel.py
+++ excel.py
@@ -1,16 +1,20 @@
 from xlrd import open_workbook
 import pprint
 
-wb = open_workbook('sample-data.xls')
 
-excel = {}
-
-for s in wb.sheets():
-    excel['sheet' + str(wb.sheets().index(s))] = {}
-    for col in xrange(s.ncols):
-        excel['sheet' + str(wb.sheets().index(s))]['col' + str(col)] = []
+def excelreaderxls(filename):
+    wb = open_workbook(filename)
+    excel = []
+    for s in wb.sheets():
         for row in xrange(s.nrows):
-            value = (s.cell(row, col).value)
-            excel['sheet' + str(wb.sheets().index(s))]['col' + str(col)].append(value)
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(excel)
+            data = []
+            for col in xrange(s.ncols):
+                value = (s.cell(row, col).value)
+                data.append(value)
+            excel.append(data)
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(excel)
+
+if __name__ == "__main__":
+    excelreaderxls('sample-data.xls')
